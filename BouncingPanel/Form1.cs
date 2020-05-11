d4d88a1e33b49622aa1bd46c7eb6766cba44bf32
Marcin Żak <marcinzak1992@gmail.com>
2020-05-11 17:54:25 +0200

    Przeskok kwadratu o 75.

--- BouncingPanel/Form1.cs
+++ BouncingPanel/Form1.cs
@@ -18,33 +18,61 @@ namespace BouncingPanel
         }
         private Random random = new Random((int)DateTime.Now.Ticks);
         private Form frm;
-        int time = 0;
+        int delta = 75;
+        int x, y;
 
         public void Form1_Load(object sender, EventArgs e)
         {
             Form frm = (Form)sender;
             this.frm = frm;
+            x = random.Next(0, SzerokoscFormyX(frm) - pnl.Width);
+            y = random.Next(0, WysokoscFormyY(frm) - pnl.Height);
+            pnl.Location = new Point(x, y);     //punkt startowy
+            PanelColor();
             timer.Enabled = true;
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            time++;
-            pnl.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
-            //pnl.Location = new Point(784,412); prawy dolny rog konsoli
-            pnl.Location = new Point(random.Next(0, WielkoscFormyX(frm)-pnl.Width), random.Next(0, WielkoscFormyY(frm)-pnl.Height));
+            x = random.Next(x - delta, x + delta);
+            y = random.Next(y - delta, y + delta);
+
+            // ponawianie wyboru współrzędnych punktu, jeśli znajduje się on poza formą
+            if(x<0)
+            {
+                    x = random.Next(0, x + delta);
+            }
+            if(x> (SzerokoscFormyX(frm) - pnl.Width))
+            {
+                    x = random.Next(x - delta, (SzerokoscFormyX(frm) - pnl.Width));
+            }
+            if (y < 0)
+            {
+                    y = random.Next(0, y + delta);
+            }
+            if (y > (WysokoscFormyY(frm) - pnl.Height))
+            {
+                    y = random.Next(y - delta, (WysokoscFormyY(frm) - pnl.Height));
+            }
+
+            pnl.Location = new Point(x, y);
+            PanelColor();
         }
-        private int WielkoscFormyX(Form frm)
+        private int SzerokoscFormyX(Form frm)
         {
             int maxX = frm.ClientSize.Width;            // uzyskujemy tylko szerokosc okna klienta (bez ramki)
 
             return maxX;
         }
-        private int WielkoscFormyY(Form frm)
+        private int WysokoscFormyY(Form frm)
         {
             int maxY = frm.ClientSize.Height;           // uzyskujemy tylko wysokosc okna klienta (bez ramki)
 
             return maxY;
+        }
+        private void PanelColor()
+        {
+            pnl.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
         }
 
     }
