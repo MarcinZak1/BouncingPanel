a7c1a8e8cdafcc5262a8c7ed77473322e30681bc
Marcin Żak <marcinzak1992@gmail.com>
2020-05-11 16:04:46 +0200

    Add project files.

---
+++ BouncingPanel/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace BouncingPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnl = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.Location = new System.Drawing.Point(412, 231);
+            this.pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(75, 75);
+            this.pnl.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 150;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(784, 412);
+            this.Controls.Add(this.pnl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ BouncingPanel/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingPanel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Random random = new Random((int)DateTime.Now.Ticks);
+        private Form frm;
+        int time = 0;
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            Form frm = (Form)sender;
+            this.frm = frm;
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            time++;
+            pnl.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+            //pnl.Location = new Point(784,412); prawy dolny rog konsoli
+            pnl.Location = new Point(random.Next(0, WielkoscFormyX(frm)-pnl.Width), random.Next(0, WielkoscFormyY(frm)-pnl.Height));
+        }
+        private int WielkoscFormyX(Form frm)
+        {
+            int maxX = frm.ClientSize.Width;            // uzyskujemy tylko szerokosc okna klienta (bez ramki)
+
+            return maxX;
+        }
+        private int WielkoscFormyY(Form frm)
+        {
+            int maxY = frm.ClientSize.Height;           // uzyskujemy tylko wysokosc okna klienta (bez ramki)
+
+            return maxY;
+        }
+
+    }
+}
